403edb403b107db35df3d5a2c54eb2553e43bdaf
kbaedal <kbaedal@gmail.com>
2017-11-10 09:45:07 +0100

    Trabajando en el BVH.

--- bvh_node.h
+++ bvh_node.h
@@ -3,6 +3,11 @@
 
 #include "hitable.h"
 #include "aabb.h"
+#include "rangen.h"
+
+int box_x_compare(const void *a, const void *b);
+int box_y_compare(const void *a, const void *b);
+int box_z_compare(const void *a, const void *b);
 
 class bvh_node : public hitable 
 {
@@ -26,13 +31,40 @@ bool bvh_node::hit(const ray &r, float tmin, float tmax, hit_record &rec) const
         bool hit_left = left->hit(r, tmin, tmax, left_rec);
         bool hit_right = right->hit(r, tmin, tmax, right_rec);
         
-        if(hit_left && hit_right) {
-            if(hit.left.t)
+        if( hit_left && hit_right ) {
+            if( left_rec.t < right_rec.t )
+                rec = left_rec;
+            else
+                rec = right_rec;
+                
+            return true;
         }
-            
-        
-        
+        else if(hit_left) {
+            rec = left_rec;
+            return true;
+        }
+        else if(hit_right) {
+            rec = right_rec;
+            return true;
+        }
+        else
+            return false;
     }
+    else
+        return false;
+}
+
+bvh_node(hitable **l, int n, float time0, float time1)
+{
+    int axis = int(3 * dis(gen));
+    
+    if( axis == 0 )
+        std::qsort(l, n, sizeof(hitable *), box_x_compare);
+    else if( axis == 1 )
+        std::qsort(l, n, sizeof(hitable *), box_y_compare);
+    else
+        std::qsort(l, n, sizeof(hitable *), box_z_compare);
+    
 }
 
 #endif // __BVH_NODE_H__
